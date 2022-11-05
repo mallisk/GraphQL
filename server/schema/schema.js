7a097bf995f0b20f153ce7a8a0edd6e18e78dc80
mallisk <malli.sk@yahoo.com>
2022-11-05 12:46:51 -0700

    created graphql schema tested with dummy data

---
+++ server/schema/schema.js
@@ -0,0 +1,38 @@
+const graphql = require("graphql");
+
+const _ = require("lodash");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+var books = [
+  { name: "book1", genre: "genre1", id: "1" },
+  { name: "book2", genre: "genre2", id: "2" },
+  { name: "book3", genre: "genre3", id: "3" },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //code for get the data from db
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
